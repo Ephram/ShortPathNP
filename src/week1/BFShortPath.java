522fc9728cbe2f1a77dc17624e5ab6fc80cf294f
Dulat.Ibrayev <ibraev.dulat@gmail.com>
2021-12-05 15:06:34 +0600

    added detecting cycle code

--- src/week1/BFShortPath.java
+++ src/week1/BFShortPath.java
@@ -1,48 +1,89 @@
 package week1;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.Set;
+
+import week4.NodeKnapsack;
 
 public class BFShortPath {
 
 	public static void main(String[] args) {
 
+//		Map<Integer, List<EdgeNode>> graph = new HashMap<Integer, List<EdgeNode>>();
+//
+//		List<EdgeNode> list0 = new ArrayList<EdgeNode>();
+//		graph.put(1, list0);
+//
+//		List<EdgeNode> list = new ArrayList<EdgeNode>();
+//		list.add(new EdgeNode(1, 2));
+//		graph.put(2, list);
+//
+//		List<EdgeNode> list2 = new ArrayList<EdgeNode>();
+//		list2.add(new EdgeNode(1, 4));
+//		list2.add(new EdgeNode(2, 1));
+//		graph.put(3, list2);
+//
+//		List<EdgeNode> list3 = new ArrayList<EdgeNode>();
+//		list3.add(new EdgeNode(2, 2));
+//		graph.put(4, list3);
+//
+//		List<EdgeNode> list4 = new ArrayList<EdgeNode>();
+//		list4.add(new EdgeNode(4, 2));
+//		list4.add(new EdgeNode(3, 4));
+//		graph.put(5, list4);
+		
+		
+		
+		File file = new File("D:\\programming\\projects\\ShortPathNP\\src\\week1\\input_random_8_4.txt"); // -36
+
 		Map<Integer, List<EdgeNode>> graph = new HashMap<Integer, List<EdgeNode>>();
 
-		List<EdgeNode> list0 = new ArrayList<EdgeNode>();
-		graph.put(1, list0);
+		List<Integer> list = new ArrayList<Integer>();
+		
+		try {
+			Scanner scanner = new Scanner(file);
+			while (scanner.hasNextLine()) {
+				String[] line = scanner.nextLine().trim().split(" ");
 
-		List<EdgeNode> list = new ArrayList<EdgeNode>();
-		list.add(new EdgeNode(1, 2));
-		graph.put(2, list);
+				if (line.length != 2) {
+					int v = Integer.parseInt(line[1]);
+					
+					if (!graph.containsKey(v)) {
+						List<EdgeNode> edgeNodeList = new ArrayList<EdgeNode>();
+						graph.put(v, edgeNodeList);
+					}
+					
+					if (graph.containsKey(v)) {
+						List<EdgeNode> nodeV = graph.get(v);
 
-		List<EdgeNode> list2 = new ArrayList<EdgeNode>();
-		list2.add(new EdgeNode(1, 4));
-		list2.add(new EdgeNode(2, 1));
-		graph.put(3, list2);
+						int headV = Integer.parseInt(line[0]);
+						int edgeCost = Integer.parseInt(line[2]);
 
-		List<EdgeNode> list3 = new ArrayList<EdgeNode>();
-		list3.add(new EdgeNode(2, 2));
-		graph.put(4, list3);
+						nodeV.add(new EdgeNode(headV, edgeCost));
+					}
 
-		List<EdgeNode> list4 = new ArrayList<EdgeNode>();
-		list4.add(new EdgeNode(4, 2));
-		list4.add(new EdgeNode(3, 4));
-		graph.put(5, list4);
+				}
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 
+		// *****************************************************//
+
+		// testing short path Bellman Ford Algo
 		BFShortPath path = new BFShortPath();
 		path.shortPath(graph);
-
 	}
 
-	public void shortPath(Map<Integer, List<EdgeNode>> graph) {
-		int ii = 0;
-		int vv = 1;
+	public Integer shortPath(Map<Integer, List<EdgeNode>> graph) {
 
-		int[][] A = new int[graph.size()][graph.size() + 1];
+		int[][] A = new int[graph.size() + 1][graph.size() + 1];
 
 		// base case
 		A[0][1] = 0;
@@ -51,7 +92,7 @@ public class BFShortPath {
 			A[0][j] = 1_000_000;
 		}
 
-		for (int i = 1; i < A.length - 1; i++) {
+		for (int i = 1; i < A.length; i++) {
 
 			Set<Integer> vertices = graph.keySet();
 
@@ -66,18 +107,21 @@ public class BFShortPath {
 				A[i][vertex] = Math.min(case1, case2);
 			}
 
-//			for (int v = 1; v < A[i].length; v++) {
-//
-//				int case1 = A[i - 1][v];
-//
-//				int w = 0;
-//				int Cwv = 0;
-//				int case2 = (A[i - 1][w]) + (Cwv);
-//
-//				A[i][v] = Math.min(case1, case2);
-//
-//			}
 		}
+
+		// detecting cycle
+		Set<Integer> vertices = graph.keySet();
+		for (Integer v : vertices) {
+			int actualresult = A[A.length - 2][v];
+			int additionalresult = A[A.length - 1][v];
+
+			if (actualresult != additionalresult) {
+				System.out.println("cycle detected ");
+				return null;
+			}
+		}
+
+		return 0;
 	}
 
 	public int getMinCase2(List<EdgeNode> list, int[][] A, int i) {
