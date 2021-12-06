ed4831f618fc729098eb236d466ecfadc839389b
Dulat.Ibrayev <ibraev.dulat@gmail.com>
2021-12-06 22:45:41 +0600

    After A lot of tests and work stable working version

--- src/week1/BFShortPath.java
+++ src/week1/BFShortPath.java
@@ -39,30 +39,35 @@ public class BFShortPath {
 //		File file = new File("D:\\programming\\projects\\ShortPathNP\\src\\week1\\input_random_8_4.txt"); // -36
 //		File file = new File("D:\\programming\\projects\\ShortPathNP\\src\\week1\\input_random_9_8.txt"); // null
 //		File file = new File("D:\\programming\\projects\\ShortPathNP\\src\\week1\\input_random_10_8.txt"); // -41
-			
-		File file = new File("D:\\programming\\projects\\ShortPathNP\\src\\week1\\input_random_14_16.txt"); // -130
-		
+
+//		File file = new File("D:\\programming\\projects\\ShortPathNP\\src\\week1\\input_random_14_16.txt"); // -130 //
+		// path 1 12
+		// 15
+
+//		File file = new File("D:\\programming\\projects\\ShortPathNP\\src\\week1\\input_random_15_16.txt"); // -100 //
+		// path 3 14
+
 //		File file = new File("D:\\programming\\projects\\ShortPathNP\\src\\week1\\input_random_20_32.txt"); // -242 X
 //		File file = new File("D:\\programming\\projects\\ShortPathNP\\src\\week1\\input_random_22_64.txt"); // -431
-		
+
 //		File file = new File("D:\\programming\\projects\\ShortPathNP\\src\\week1\\input_random_21_64.txt"); // null
-		
+
 //		File file = new File("D:\\programming\\projects\\ShortPathNP\\src\\week1\\input_random_30_256.txt"); // -961
-		
+
+//		File file = new File("D:\\programming\\projects\\ShortPathNP\\src\\week1\\input_random_39_1024.txt"); // -2123
+
 //		File file = new File("D:\\programming\\projects\\ShortPathNP\\src\\week1\\input_random_40_1024.txt"); // -2361
-		
+
 //		File file = new File("D:\\programming\\projects\\ShortPathNP\\src\\week1\\input_random_44_2048.txt"); // -3127
-			
-//		File file = new File("D:\\programming\\projects\\ShortPathNP\\src\\week1\\t1case.txt"); // -2
-		
+
+		File file = new File("D:\\programming\\projects\\ShortPathNP\\src\\week1\\t1case.txt"); // -2
+
 //		File file = new File("D:\\programming\\projects\\ShortPathNP\\src\\week1\\t2case.txt"); // null
 
 //		File file = new File("D:\\programming\\projects\\ShortPathNP\\src\\week1\\g1.txt"); // null
 //		File file = new File("D:\\programming\\projects\\ShortPathNP\\src\\week1\\g2.txt"); //null
 //		File file = new File("D:\\programming\\projects\\ShortPathNP\\src\\week1\\g3.txt"); // -12
 
-		
-		
 		Map<Integer, List<EdgeNode>> graph = new HashMap<Integer, List<EdgeNode>>();
 
 		List<Integer> list = new ArrayList<Integer>();
@@ -106,14 +111,14 @@ public class BFShortPath {
 	}
 
 	public Integer shortPath(Map<Integer, List<EdgeNode>> graph, int numberofVertices) {
-
+		int minmin = 1_000_000_000;
 		int[][] A = new int[numberofVertices + 1][numberofVertices + 1];
 
 		// base case
 		A[0][1] = 0;
 
 		for (int j = 2; j < A[0].length; j++) {
-			A[0][j] = 1_000_000;
+			A[0][j] = 1_000_000_000;
 		}
 
 		for (int i = 1; i < A.length; i++) {
@@ -129,30 +134,83 @@ public class BFShortPath {
 				int case2 = getMinCase2(comingVerticesList, A, i);
 
 				A[i][vertex] = Math.min(case1, case2);
-			}
+				if (minmin > A[i][vertex]) {
+					minmin = A[i][vertex];
+					System.out.println(minmin + " minimal per round " + vertex + " vertex");
 
+				}
+			}
 		}
 
 		// detecting cycle
 		Set<Integer> vertices = graph.keySet();
-		int min = 1_000_000;
+		int min = 1_000_000_000;
+		int saveV = 0;
 		for (Integer v : vertices) {
 			int actualresult = A[A.length - 2][v];
 			int additionalresult = A[A.length - 1][v];
-			min = Math.min(min, actualresult);
-			
+
+			if (actualresult < min) {
+				saveV = v;
+				min = actualresult;
+			}
 			if (actualresult != additionalresult) {
 				System.out.println("cycle detected ");
 				return null;
 			}
 		}
-		
-		
-		
+
+		List<Integer> pathList = reverseDetection(A, numberofVertices, saveV, graph);
+		System.out.println(" path list " + pathList.size());
+		for (int i = 0; i < pathList.size(); i++) {
+			System.out.println(pathList.get(i));
+		}
 		return min;
 	}
-	
-	
+
+	public List<Integer> reverseDetection(int[][] A, int i, int v, Map<Integer, List<EdgeNode>> graph) {
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(v);
+		int minmin = -1_000_000_000;
+
+		try {
+			while (graph.get(v).isEmpty() != true) {
+
+				List<EdgeNode> edgeNodes = graph.get(v);
+
+				int w = edgeNodes.get(0).getV();
+				int Cwv = edgeNodes.get(0).getEdgeCost();
+				int min = (A[i - 1][w]) + (Cwv);
+
+				for (int ii = 1; ii < edgeNodes.size(); ii++) {
+					int w2 = edgeNodes.get(ii).getV();
+					int Cwv2 = edgeNodes.get(ii).getEdgeCost();
+					int min2 = (A[i - 1][w2]) + (Cwv2);
+
+					if (min > min2) {
+						w = w2;
+						min = min2;
+
+						if (min > minmin) {
+							minmin = min;
+						}
+					}
+
+				}
+				v = w;
+//				i--;
+				System.out.println(i);
+				System.out.println(w);
+				list.add(w);
+				edgeNodes = null;
+			}
+		} catch (NullPointerException e) {
+			System.out.println("catched");
+		}
+
+		return list;
+	}
+
 	public int getMinCase2(List<EdgeNode> list, int[][] A, int i) {
 
 		if (!list.isEmpty()) {
@@ -162,9 +220,9 @@ public class BFShortPath {
 			int min = (A[i - 1][w]) + (Cwv);
 
 			for (int ii = 1; ii < list.size(); ii++) {
-				w = list.get(ii).getV();
-				Cwv = list.get(ii).getEdgeCost();
-				int min2 = (A[i - 1][w]) + (Cwv);
+				int w2 = list.get(ii).getV();
+				int Cwv2 = list.get(ii).getEdgeCost();
+				int min2 = (A[i - 1][w2]) + (Cwv2);
 
 				min = Math.min(min, min2);
 
